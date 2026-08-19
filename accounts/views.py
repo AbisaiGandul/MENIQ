2698e17ddb86a1c06ac78569f6c6debe37fe9ce7
Abisai <abisaigandul64@gmail.com>
2026-08-18 22:15:15 -0300

    Conexion de Base de Datos y Sistema de Registro
    Hice la conexion y migracion de la base de datos llamada 'resto'.
    Cree los primeros pasos para que un usuario se registre y se almacene en
    la base de datos.

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-        return redirect('/index')
+        form = request.POST
+        user = User.objects.create_user(username= form['username'], email = form['email'], password = form['password'])
+        return render(request, 'index.html', {'user' : user.username})
     return render(request, 'login.html')
 
 def index(request):
